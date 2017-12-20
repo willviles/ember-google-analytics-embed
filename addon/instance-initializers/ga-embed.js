65d82639724859da82c4ec6fe120dd6a2d97cccf
Will Viles <will@willviles.com>
2017-12-20 13:55:28 +0000

    Removes instance intializer in favour of using service init function

--- addon/instance-initializers/ga-embed.js
+++
@@ -1,9 +0,0 @@
-export function initialize(appInstance) {
-  let gaEmbedService = appInstance.lookup('service:ga-embed');
-  gaEmbedService.initialize();
-}
-
-export default {
-  name: 'ga-embed',
-  initialize
-};
